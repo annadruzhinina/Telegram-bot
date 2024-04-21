bfcd64ae2307b85d07a69b51c79feab836d71f4c
anna <anna@rcbd.org>
2024-04-21 01:01:55 -0400

    Pascals Triangle

--- index.js
+++ index.js
@@ -3,13 +3,44 @@ const { Bot, GrammyError, HttpError } = require("grammy");
 
 const bot = new Bot(process.env.BOT_API_KEY);
 
-bot.command("start", async (ctx) => {
+bot.api.setMyCommands([
+  { command: "start", description: "Bot starting" },
+  { command: "hello", description: "Get Hello" },
+]);
+
+bot.command("start:text", async (ctx) => {
   await ctx.reply(
     `Hey there! I'm a simple telegram bot made using grammy and nodejs.\n\n`
   );
 });
-bot.on("message", async (ctx) => {
+// bot.on("message", async (ctx) => {
+// bot.on("message:photo", async (ctx) => {
+// bot.on("message:voice", async (ctx) => {
+// bot.on("message:entities:url", async (ctx) => {
+bot.on("::email", async (ctx) => {
+  // bot.on(":photo").on("::hashtag", ()=>{}) =>
+
   await ctx.reply("Need think for a bit...");
+});
+
+
+//✅ Make my filter
+bot.on("msg").filter(
+  (ctx) => {
+    return ctx.from.id === 970767623;
+  },
+  async (ctx) => {
+    await ctx.reply("Hey,Admin");
+  }
+);
+
+bot.hears("ping", async (ctx) => {
+  await ctx.reply("pong");
+});
+
+// ctx- context
+bot.command(["say_hello", "hello", "say_hi"], async (ctx) => {
+  await ctx.reply(`Hello`);
 });
 
 bot.catch((err) => {
